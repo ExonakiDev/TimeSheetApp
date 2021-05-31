23034bcd0cfb3b2cf13b5490c81bfd5cba2cace1
exonakidev <rsanyal17@yahoo.co.in>
2021-05-31 16:56:08 +0530

    added regiter route, DB Class

--- app.py
+++ app.py
@@ -1,23 +1,46 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session
 from flaskext.mysql import MySQL
+import pymysql
 
 # init flask app
 app = Flask(__name__)
+app.config['TESTING'] = False
+# session secret key
+app.secret_key = b'L\xdb\x1b\xc2\xa9\xbc\xc0\x84\xcb\xb67\x0c\xdf#hW'
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 
 # init MySQL database
 mysql = MySQL()
 mysql.init_app(app)
 
+
 # make cursor to db
 # cursor = mysql.get_db().cursor()
 
-# Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
+class Database:
+    def _init_(self):
+        host = "127.0.0.1"
+        user = "root"
+        password = "Minusb@12"
+        db = "demo"
+        self.con = pymysql.connect(host=host, user=user, password=password, db=db,
+                                   cursorclass=pymysql.cursors.DictCursor)
+        self.cur = self.con.cursor()
+
+    def list_products(self):
+        self.cur.execute("SELECT PID, Name, Price FROM products WHERE PID='1'")
+        result = self.cur.fetchall()
+        return result
+
 
 @app.route("/")
-@login_required
 def index():
-
+    ## Placeholder
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return 'You are not logged in'
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -41,8 +64,20 @@ def login():
         return render_template("login.html")
 
 @app.route("/logout")
-@login_required
 def logout():
+    # session.pop('username', None)
     session.clear()
     # redirect to main page
     return redirect("/")
+
+@app.route("/register")
+def register():
+    if request.method == "POST":
+        if not request.form.get("username"):
+            # Return error message TODO
+            return error
+        elif not request.form.get("password"):
+            # Return error message TODO
+            return error
+    else:
+        return render_template("register.html")

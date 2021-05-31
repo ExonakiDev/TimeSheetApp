e49f7ae68d0210fbf6046125f7854a225ddb1891
exonakidev <rsanyal17@yahoo.co.in>
2021-05-31 16:31:05 +0530

    added basic index, login, logout routes

--- app.py
+++ app.py
@@ -1,7 +1,48 @@
 from flask import Flask
+from flaskext.mysql import MySQL
 
+# init flask app
 app = Flask(__name__)
 
+# init MySQL database
+mysql = MySQL()
+mysql.init_app(app)
+
+# make cursor to db
+# cursor = mysql.get_db().cursor()
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@login_required
+def index():
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    # If user reached route via post method
+    if request.method == "POST":
+        if not request.form.get("username"):
+            # Return error message TODO
+            return error
+        elif not request.form.get("password"):
+            # Return error message TODO
+            return error
+
+        # TODO Check login
+
+        # redirect to main page
+        return redirect("/")
+    # User reached route via get method
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    session.clear()
+    # redirect to main page
+    return redirect("/")

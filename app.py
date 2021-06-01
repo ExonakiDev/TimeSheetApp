ca0a45dd91f967f495961cd39ff25a2fbf1001da
exonakidev <rsanyal17@yahoo.co.in>
2021-06-01 14:54:26 +0530

    Added mysql database connectivity, login functions, and session cookies

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, flash, redirect, render_template, request, session
+import sys
+from flask import Flask, flash, redirect, render_template, request, url_for, session
 from flaskext.mysql import MySQL
+from flask_bcrypt import Bcrypt
+from flask_session import Session
 from database import Database
 import pymysql
 
@@ -10,6 +13,12 @@ app.config['TESTING'] = False
 app.secret_key = b'L\xdb\x1b\xc2\xa9\xbc\xc0\x84\xcb\xb67\x0c\xdf#hW'
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Password Hashing
+bcrypt = Bcrypt(app)
 
 
 # init MySQL database
@@ -22,30 +31,55 @@ mysql.init_app(app)
 @app.route("/")
 def index():
     ## Placeholder
-    if 'username' in session:
-        return f'Logged in as {session["username"]}'
-    return 'You are not logged in'
+    if not session.get("user_id"):
+        return redirect("/login")
+    return "Logged in"
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    """ User logs in here """
     error = None
-    session.clear()
+    # Forget past user ids
+    # session.clear()
+    #Call DB class to init database
+    db=Database()
+
     # If user reached route via post method
     if request.method == "POST":
-        if not request.form.get("username"):
+        username = request.form.get("id")
+        password = request.form.get("password")
+        # print(username, password)
+        if not username:
             # Return error message TODO
-            error = "Please enter username!"
-            return render_template('login.html', error=error)
-        elif not request.form.get("password"):
+            error = "Please enter Employee ID!"
+            # return render_template('login.html', error=error)
+            flash("Please enter Employee ID!")
+            return redirect(url_for("login"))
+        elif not password:
             # Return error message TODO
             error = "Please enter password!"
-            return render_template('login.html', error=error)
-        # TODO Check login
+            # return render_template('login.html', error=error)
+            flash("Please enter password!")
+            return redirect(url_for("login"))
 
-
+        # Check login
+        try:
+            rows = db.check_credentials(username)
+            print(rows, file=sys.stderr)
+            if len(rows)!=1 or not (rows[0]["password"] == password):
+                error = "Invalid credentials"
+                flash("Invalid credentials")
+                return redirect(url_for("login"))
+            # Remember which user has logged in
+            session["user_id"] = rows[0]["employee_id"]
+        finally:
+            # db.close_cursor()
+            pass
         # redirect to main page
+        flash('You were successfully logged in')
         return redirect("/")
+
     # User reached route via get method
     else:
         return render_template("login.html")
@@ -68,3 +102,7 @@ def register():
             return error
     else:
         return render_template("register.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- database.py
+++ database.py
@@ -1,16 +1,25 @@
 import pymysql
 
 class Database:
-    def _init_(self):
+    def __init__(self):
         host = "127.0.0.1"
         user = "root"
-        password = "Minusb@12"
-        db = "demo"
+        password = "toor"
+        db = "timesheet"
         self.con = pymysql.connect(host=host, user=user, password=password, db=db,
                                    cursorclass=pymysql.cursors.DictCursor)
         self.cur = self.con.cursor()
 
-    def list_products(self):
-        self.cur.execute("SELECT PID, Name, Price FROM products WHERE PID='1'")
-        result = self.cur.fetchall()
-        return result
+    # def list_products(self):
+    #     self.cur.execute("SELECT PID, Name, Price FROM products WHERE PID='1'")
+    #     result = self.cur.fetchall()
+    #     return result
+
+    def check_credentials(self, username):
+        sql = "SELECT * from Login WHERE employee_id = %s"
+        self.cur.execute(sql, username)
+        rows = self.cur.fetchall()
+        return rows
+
+    # def close_cursor(self):
+    #     self.con.close()

9848e368e38eb704881b6f1588f5dd7e69a8fee8
exonakidev <rsanyal17@yahoo.co.in>
2021-06-05 05:08:41 +0530

    updated timesheet javascript

--- static/timesheetjs.js
+++ static/timesheetjs.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
       $('#datePicker').datepicker({ //initiate JQueryUI datepicker
         showAnim: 'fadeIn',
         dateFormat: "dd/mm/yy",
-        firstDay: 1, //first day is Monday
+        firstDay: 6, //first day is Monday
         beforeShowDay: function(date) {
           //only allow Mondays to be selected
-          return [date.getDay() == 1,""];
+          return [date.getDay() == 6,""];
         },
         onSelect: populateDates
       });
@@ -19,7 +19,7 @@ $(document).ready(function(){
         $('.bottom').removeClass('d-none'); //display total hours worked
         let chosenDate = $('#datePicker').datepicker('getDate'); //get chosen date from datepicker
         let newDate;
-        const monStartWeekDays = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+        const monStartWeekDays = ['Saturday','Sunday','Monday','Tuesday','Wednesday','Thursday','Friday'];
         for(let i = 0; i < weekDays.length; i++) { //iterate through each weekday
           newDate = new Date(chosenDate); //create date object
           newDate.setDate(chosenDate.getDate() + i); //increment set date
@@ -28,21 +28,17 @@ $(document).ready(function(){
           <tr>
             <td class="day">${weekDays[newDate.getDay()].slice(0,3)}</td>
             <td class="date">${newDate.getDate()} / ${newDate.getMonth() + 1} / ${newDate.getFullYear()}</td>
-            <td class="start-time"><input id="startTime${monStartWeekDays[i]}" class="time ui-timepicker-input" type="text" /></td>
-            <td class="finish-time"><input id="finishTime${monStartWeekDays[i]}" class="time ui-timepicker-input" type="text" /></td></td>
+            <td><input type="hidden" name="td${i + 1}" value="${newDate.getDate()}/${newDate.getMonth() + 1}/${newDate.getFullYear()}"></td>
             <td class="break">
               <select id="break${monStartWeekDays[i]}">
                 <option value="1">Present</option>
-                <option value="0.5">Leave</option>
-                <option value="0">Holiday</option>
+                <option value="0">Leave</option>
+                <option value="2">Half-Day</option>
               </select>
             </td>
-            <td class="hours-worked" id="hoursWorked${monStartWeekDays[i]}">
-              0
-            </td>
+            <td><input type="hidden" id="stat${monStartWeekDays[i]}" name="status${monStartWeekDays[i]}" value="1"></td>
           </tr>
           ` );
-
           //function to calculate hours worked
           let calculateHours = () => {
             let startVal = $(`#startTime${monStartWeekDays[i]}`).val();
@@ -51,8 +47,9 @@ $(document).ready(function(){
             let finishTime = new Date( `01/01/2007 ${finishVal}` );
             let breakTime = $(`#break${monStartWeekDays[i]}`).val();
             let hoursWorked = (finishTime.getTime() - startTime.getTime()) / 1000;
+            let status = $(`#break${monStartWeekDays[i]}`).val();
             hoursWorked /= (60 * 60);
-
+            $(`#stat${monStartWeekDays[i]}`).val(status);
 
             if (startVal && finishVal && breakTime=="1") { //providing both start and finish times are set
               if (hoursWorked >= 0) { //if normal day shift
@@ -67,7 +64,6 @@ $(document).ready(function(){
               $(`#hoursWorked${monStartWeekDays[i]}`).html(hoursWorked);
             }
 
-            updateTotal();
           }
           //initiate function whenever an input value is changed
           $(`#startTime${monStartWeekDays[i]}, #finishTime${monStartWeekDays[i]}, #break${monStartWeekDays[i]}`).on('change', calculateHours);
@@ -76,14 +72,7 @@ $(document).ready(function(){
         $('.start-time input').timepicker({ 'timeFormat': 'H:i', 'step': 15, 'scrollDefault': '09:00' });
         $('.finish-time input').timepicker({ 'timeFormat': 'H:i', 'step': 15, 'scrollDefault': '17:00' });
 
-        function updateTotal() { //function to update the total hours worked
-          let totalHoursWorked = 0;
-          let hrs = document.querySelectorAll('.hours-worked');
-          hrs.forEach(function(val) {
-            totalHoursWorked += Number(val.innerHTML);
-          });
-          document.querySelector('#totalHours').innerHTML = totalHoursWorked;
-        }
+
 
 
       }
